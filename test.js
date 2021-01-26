377a00ecb4ad15142fff2ceaa6edd808f3c75847
Nixon <CMPCNIXO@ljmu.ac.uk>
2021-01-26 16:33:14 +0000

    Initial commit

---
+++ test.js
@@ -0,0 +1,11 @@
+//first NodeJS application
+constexpress=require('express')
+constapp=express()
+constport=3000
+app.get('/',(req,res)=>{
+    res.send('Hello Virtual Machine!')
+})
+
+app.listen(port,()=>{
+    console.log(`Express Application  listening at port 3000`)
+})
